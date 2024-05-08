4f83cee549779a2cabc7b7c91521b704c9c38a03
devgmu <dev@georgemurphymusic.com>
2024-05-08 17:57:37 +0100

    added pink border to benefits

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -17,7 +17,7 @@ const Benefits = () => {
         <div className="flex flex-wrap gap-10 mb-10">
           {benefits.map((item) => (
             <div
-              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem] border-2 border-pink-800 rounded-md "
               style={{
                 backgroundImage: `url(${item.backgroundUrl})`,
               }}
@@ -26,7 +26,7 @@ const Benefits = () => {
               <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
                 <h5 className="h5 mb-5">{item.title}</h5>
                 <p className="body-2 mb-6 text-n-3">{item.text}</p>
-                
+
                 <div className="flex items-center mt-auto">
                   <img
                     src={item.iconUrl}
@@ -47,7 +47,7 @@ const Benefits = () => {
               {item.light && <GradientLight />}
 
               <div
-                className="absolute inset-0.5 bg-n-8 opacity-75 "
+                className="absolute inset-0.5 bg-n-8 opacity-50 "
                 style={{ clipPath: "url(#benefits)" }}
               >
                 <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
