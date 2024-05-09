0036f40ac0da3f9b6bb233c39a72ac22780b36e3
devgmu <dev@georgemurphymusic.com>
2024-05-09 11:03:44 +0100

    added tagline to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -36,8 +36,7 @@ const Hero = () => {
             </span>
           </h1>
           <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
-            Unleash the power of AI within Brainwave. Upgrade your productivity
-            with Brainwave, the open AI chat app.
+          Barber Booking with Smarter, Faster Solutions.
           </p>
           <Button href="#kolOpenContestBox" white>
             Join the Waitlist
